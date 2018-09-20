27e86c9791169c262512b163cc880b04575f2839
Tegger <42648199+Tegger@users.noreply.github.com>
2018-09-20 11:08:30 -0500

    Add files via upload

---
+++ classes/src/Employee.java
@@ -0,0 +1,87 @@
+ public class Employee {
+        private int ID;
+        private String firstName;
+        private String lastName;
+        private String emailAddress;
+        private String homePhone;
+        private String workPhone;
+        private String cellPhone;
+
+        public Employee(int ID, String firstName, String lastName, String emailAddress, String homePhone, String workPhone, String cellPhone){
+            this.ID = ID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.homePhone = homePhone;
+            this.workPhone = workPhone;
+            this.cellPhone = cellPhone;
+        }
+
+        public int getID(){
+            return ID;
+        }
+
+        public void setID(int ID){
+            this.ID = ID;
+        }
+
+        public String getFirstName(){
+            return firstName;
+        }
+
+        public void setFirstName(String firstName){
+            this.firstName = firstName;
+        }
+
+        public String getLastName(){
+            return lastName;
+        }
+
+        public void setLastName(String lastName){
+            this.lastName = lastName;
+        }
+
+        public String getEmailAddress(){
+            return emailAddress;
+        }
+
+        public void setEmailAddress(String emailAddress){
+            this.emailAddress = emailAddress;
+        }
+        public String getHomePhone(){
+            return homePhone;
+        }
+
+        public void setHomePhone(String homePhone){
+            this.homePhone = homePhone;
+        }
+        public String getfirstName(){
+            return firstName;
+        }
+
+        public void setID(String firstName){
+            this.firstName = firstName;
+        }
+        public String getWorkPhone(){
+            return workPhone;
+        }
+
+        public void setWorkPhone(String workPhone){
+            this.workPhone = workPhone;
+        }
+        public String getCellPhone(){
+            return cellPhone;
+        }
+
+        public void setCellPhone(String cellPhone){
+            this.cellPhone = cellPhone;
+        }
+        public void printAll(){
+            System.out.println("ID Number: " + ID + "\nFirst Name: " + firstName + "\nLast Name" + lastName + "\n" + "\nEmail: " + emailAddress + "\nHome Phomne: "
+                    + homePhone + "\nWork Phone: " + workPhone + "\nCell Phone: " + cellPhone);
+        }
+
+
+    }
+
+
---
+++ classes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String args[]){
+
+        Employee person1 = new Employee(1,"John", "Arbuckle", "Jarbuckle@yahoo.com", "815-245-6774",
+                "247-856-2234", "765-324-0998");
+
+        person1.printAll();
+
+    }
+
+
+
+
+}
